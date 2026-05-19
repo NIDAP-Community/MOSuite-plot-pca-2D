61eb27f3ed177277625bff0eb7ba0fd30ef7a07d
Kelly Sovacool <kelly.sovacool@nih.gov>
2026-05-19 13:34:43 -0400

    refactor: load local MOSuite

--- code/main.R
+++ code/main.R
@@ -2,10 +2,10 @@
 rlang::global_entrace()
 library(argparse)
 library(glue)
-library(MOSuite)
 library(readr)
 library(stringr)
 library(dplyr)
+devtools::load_all('/code/MOSuite')
 
 # set up capsule environment
 setup_capsule_environment()
@@ -13,22 +13,97 @@ setup_capsule_environment()
 # parse CLI arguments
 parser <- ArgumentParser()
 
-parser$add_argument("--count_type", type="character", default="filt")
-parser$add_argument("--sub_count_type", type="character", default=NULL, help="Sub count type if count_type is a list")
-parser$add_argument("--feature_id_colname", type="character", default=NULL, help="Column name for feature IDs")
-parser$add_argument("--sample_id_colname", type="character", default=NULL, help="Column name for sample IDs")
-parser$add_argument("--samples_to_rename", type="character", default="", help="Samples to rename in format old:new,old2:new2")
-parser$add_argument("--group_colname", type="character", default="Group", help="Column name for sample groups")
-parser$add_argument("--label_colname", type="character", default="Label", help="Column name for sample labels")
-parser$add_argument("--principal_components", type="character", default="1,2", help="Principal components to plot (comma-separated)")
-parser$add_argument("--legend_position", type="character", default="top", help="Position of the legend")
-parser$add_argument("--point_size", type="double", default=1, help="Size of points in plot")
-parser$add_argument("--add_label", type="logical", default=TRUE, help="Whether to add labels to points")
-parser$add_argument("--label_font_size", type="double", default=3, help="Font size for labels")
-parser$add_argument("--label_offset_x_", type="double", default=2, help="X-axis offset for labels")
-parser$add_argument("--label_offset_y_", type="double", default=2, help="Y-axis offset for labels")
-parser$add_argument("--interactive_plots", type="logical", default=FALSE, help="Whether to create interactive plots")
-parser$add_argument("--color_values", type="character", default="#5954d6,#e1562c,#b80058,#00c6f8,#d163e6,#00a76c,#ff9287,#008cf9,#006e00,#796880,#FFA500,#878500", help="Comma-separated color values")
+parser$add_argument("--count_type", type = "character", default = "filt")
+parser$add_argument(
+  "--sub_count_type",
+  type = "character",
+  default = NULL,
+  help = "Sub count type if count_type is a list"
+)
+parser$add_argument(
+  "--feature_id_colname",
+  type = "character",
+  default = NULL,
+  help = "Column name for feature IDs"
+)
+parser$add_argument(
+  "--sample_id_colname",
+  type = "character",
+  default = NULL,
+  help = "Column name for sample IDs"
+)
+parser$add_argument(
+  "--samples_to_rename",
+  type = "character",
+  default = "",
+  help = "Samples to rename in format old:new,old2:new2"
+)
+parser$add_argument(
+  "--group_colname",
+  type = "character",
+  default = "Group",
+  help = "Column name for sample groups"
+)
+parser$add_argument(
+  "--label_colname",
+  type = "character",
+  default = "Label",
+  help = "Column name for sample labels"
+)
+parser$add_argument(
+  "--principal_components",
+  type = "character",
+  default = "1,2",
+  help = "Principal components to plot (comma-separated)"
+)
+parser$add_argument(
+  "--legend_position",
+  type = "character",
+  default = "top",
+  help = "Position of the legend"
+)
+parser$add_argument(
+  "--point_size",
+  type = "double",
+  default = 1,
+  help = "Size of points in plot"
+)
+parser$add_argument(
+  "--add_label",
+  type = "logical",
+  default = TRUE,
+  help = "Whether to add labels to points"
+)
+parser$add_argument(
+  "--label_font_size",
+  type = "double",
+  default = 3,
+  help = "Font size for labels"
+)
+parser$add_argument(
+  "--label_offset_x_",
+  type = "double",
+  default = 2,
+  help = "X-axis offset for labels"
+)
+parser$add_argument(
+  "--label_offset_y_",
+  type = "double",
+  default = 2,
+  help = "Y-axis offset for labels"
+)
+parser$add_argument(
+  "--interactive_plots",
+  type = "logical",
+  default = FALSE,
+  help = "Whether to create interactive plots"
+)
+parser$add_argument(
+  "--color_values",
+  type = "character",
+  default = "#5954d6,#e1562c,#b80058,#00c6f8,#d163e6,#00a76c,#ff9287,#008cf9,#006e00,#796880,#FFA500,#878500",
+  help = "Comma-separated color values"
+)
 
 args <- parser$parse_args()
 
@@ -37,21 +112,23 @@ moo <- load_moo_from_data_dir()
 
 # run MOSuite
 plot_pca_2d(
-    moo,
-    count_type = args$count_type,
-    sub_count_type = args$sub_count_type,
-    principal_components = as.integer(parse_optional_vector(args$principal_components)),
-    feature_id_colname = args$feature_id_colname,
-    sample_id_colname = args$sample_id_colname,
-    samples_to_rename = parse_samples_to_rename(args$samples_to_rename),
-    group_colname = args$group_colname,
-    label_colname = args$label_colname,
-    legend_position = args$legend_position,
-    point_size = args$point_size,
-    add_label = args$add_label,
-    label_font_size = args$label_font_size,
-    label_offset_x_ = args$label_offset_x_,
-    label_offset_y_ = args$label_offset_y_,
-    interactive_plots = args$interactive_plots,
-    color_values = parse_optional_vector(args$color_values)
+  moo,
+  count_type = args$count_type,
+  sub_count_type = args$sub_count_type,
+  principal_components = as.integer(parse_optional_vector(
+    args$principal_components
+  )),
+  feature_id_colname = args$feature_id_colname,
+  sample_id_colname = args$sample_id_colname,
+  samples_to_rename = parse_samples_to_rename(args$samples_to_rename),
+  group_colname = args$group_colname,
+  label_colname = args$label_colname,
+  legend_position = args$legend_position,
+  point_size = args$point_size,
+  add_label = args$add_label,
+  label_font_size = args$label_font_size,
+  label_offset_x_ = args$label_offset_x_,
+  label_offset_y_ = args$label_offset_y_,
+  interactive_plots = args$interactive_plots,
+  color_values = parse_optional_vector(args$color_values)
 )
